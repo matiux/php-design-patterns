1d818c0deabb0af084ad1f64e7d1c28f2322b17d
Matiux <m.galacci@gmail.com>
2018-07-06 12:01:45 +0200

    Aggiunto menu

--- Iterator/DinerMerger/client.php
+++ Iterator/DinerMerger/client.php
@@ -2,14 +2,15 @@
 
 require __DIR__ . '/../../vendor/autoload.php';
 
+use DesignPatterns\Iterator\DinerMerger\CafeMenu;
 use DesignPatterns\Iterator\DinerMerger\DinerMenu;
 use DesignPatterns\Iterator\DinerMerger\PancakeHouseMenu;
 use DesignPatterns\Iterator\DinerMerger\Waitress;
 
-
 $pancakeHouseMenu = new PancakeHouseMenu();
 $dinerMenu = new DinerMenu();
+$cafeMenu = new CafeMenu();
 
-$waitress = new Waitress($pancakeHouseMenu, $dinerMenu);
+$waitress = new Waitress($pancakeHouseMenu, $dinerMenu, $cafeMenu);
 
 $waitress->printMenu();
